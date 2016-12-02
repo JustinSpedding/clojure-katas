921d4791bc680ca99213e0ca9b2c67e7751f2115
Justin Spedding <justinspedding@users.noreply.github.com>
2016-12-02 15:23:34 -0500

    Completed count-coin-change kata

--- src/clojure_katas/count_coin_change.clj
+++ src/clojure_katas/count_coin_change.clj
@@ -3,6 +3,14 @@
 
 (def coins [1 5 10 25 50])
 
+(defn number-of-ways [sum coins]
+  (cond
+    (empty? coins) 0
+    (neg? sum) 0
+    (zero? sum) 1
+    (pos? sum) (+ (number-of-ways (- sum (first coins)) coins)
+                  (number-of-ways sum (rest coins)))))
+
 (core/defproblem count-coin-change-tree
   "
   Reference: SICP
@@ -13,4 +21,5 @@
   # of ways to change amount a using n kinds of coins =
     # of ways to change amount a using all but first kind of coin +
     # of ways to change amount a - d, where d is the denomination of the first kind of coin"
-  [amount, index-of-coin-change-array])
+  [amount, index-of-coin-change-array]
+  (number-of-ways amount (into '() coins)))
